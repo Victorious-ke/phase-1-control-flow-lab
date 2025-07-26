f8aa13559e1b5f6f6d71645a4e095ddfab318ca1
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-07-26 16:46:44 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!";
+  }
+  else if (distance > 400 && distance <= 2000) {
+    result = "That will be twenty bucks.";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else {
+    result = "No can do!"
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(nameOfCity) {
+  let result
+  if (nameOfCity = "NYC") {
+    result = "Ok, sounds good"
+  }
+  else {
+    result = "No go."
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
